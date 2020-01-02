5fb3626586237bacdbea03b95c624564ea05dd2c
Abraham Mathews <59157038+AbrahamMathews97@users.noreply.github.com>
2020-01-02 13:54:20 +0530

    Add files via upload
    Assignment to create file batch.txt and list the directories and list files in the subfolders..

---
+++ File_Batch_Creation.java
@@ -0,0 +1,46 @@
+package InputOutput;
+
+import java.io.File;
+import java.io.IOException;
+//import java.util.List;
+//import java.util.ArrayList;
+import java.util.Scanner;
+
+public class File_Batch_Creation {
+   
+	public static void main(String[] args) throws IOException {
+		File file=new File("C:\\Users\\Admin\\Desktop\\java\\Batch\\BatchMates.txt");
+		File directory=new File("C:\\\\Users\\\\Admin\\\\Desktop\\\\java\\\\Batch\\\\");
+		File in_directory=new File("C:\\Users\\Admin\\Desktop\\java\\Batch\\batch");
+		
+		if(file.exists() || directory.exists())
+		{
+			System.out.println("Directory already exists");
+		//	file.delete();
+		//	directory.delete();
+			
+		}
+		else
+		{   
+			directory.mkdirs();
+			in_directory.mkdirs();
+			file.createNewFile();
+			System.out.println("Directory created");
+			System.out.println("File Batch.txt created ");
+		}
+		
+		File[] flist=directory.listFiles();
+		for(File f : flist)
+		{
+			if(f.isFile())
+			{
+				System.out.println(f.getName()+" is a File");
+			} 
+			else if(f.isDirectory())
+			{
+				System.out.println(f.getName()+" is a directory");
+			}
+		}
+	}
+		
+}
